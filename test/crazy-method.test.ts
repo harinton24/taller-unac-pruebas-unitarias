dffeda0282e2a5df027baf96a196f9a0216df3f1
Harinton <harintond.arizav@unac.edu.co>
2022-04-06 20:42:43 -0500

    New Cazy Methods Tests

--- test/crazy-method.test.ts
+++ test/crazy-method.test.ts
@@ -5,3 +5,14 @@ test(" Test crazy Method", ()=>{
     expect(crazy(5,2)).toBe(10);
 
 });
+
+test(" Test crazy ", ()=>{
+    
+    expect(crazy(5,3)).toBeGreaterThanOrEqual(11);
+
+});
+test(" Test crazy ", ()=>{
+    
+    expect(crazy(10,5)).toBeLessThan(19);
+
+});

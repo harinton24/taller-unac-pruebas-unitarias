d00ba336de0328c7203042a1b3574a8d5c854190
yppabonf <yuletsyp.pabonf@unac.edu.co>
2022-04-06 20:35:35 -0500

    changes in sum an mean arrays

--- test/sum-array.test.ts
+++ test/sum-array.test.ts
@@ -1,15 +1,15 @@
 import {sum} from '../src/sum-array';
 
-test('Test sum array to be 5', () => {
+test('Test sum array to be 4', () => {
     expect(sum([2,2])).toBe(4);
 
 });
 
-test('Test sum array to be 5', () => {
+test('Test sum array to be 52', () => {
     expect(sum([50,2])).toBe(52);
 
 });
-test('Test sum array to be 5', () => {
+test('Test sum array to be 11.5', () => {
     expect(sum([9.5,2])).toBe(11.5);
 
 });
